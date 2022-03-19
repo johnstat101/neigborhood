09d7c1e97868aaa163d80da2aeec6b1d862bea9a
johnstat101 <johnjorogek@gmail.com>
2022-03-19 16:10:20 +0300

    start project neighborhood

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn projectreviews.wsgi
---
+++ myneighborhood/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MyneighborhoodConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myneighborhood'
